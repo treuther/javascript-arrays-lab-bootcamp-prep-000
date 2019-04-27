0a3ead2b29b60a3003a189dc5d90b3b78c3164c2
Tracy Reuther <tracyreuther@gmail.com>
2019-04-27 10:39:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function destructivelyAppendKitten(name) {
  return kittens
 }
 
-function destructivelyPrependKittens(name) {
+function destructivelyPrependKitten(name) {
   kittens.unshift(name)
   return kittens
 }
