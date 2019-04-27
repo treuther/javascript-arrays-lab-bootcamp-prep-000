d0831a72ed19d31df024d5d98d03f6aafcda9053
Tracy Reuther <tracyreuther@gmail.com>
2019-04-27 10:41:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ function destructivelyRemoveLastKitten(name){
   kittens.pop(name)
   return kittens
 }
+
+function destructivelyRemoveFirstKitten(name {
+  kittens.unshift(name)
+  return kittens
+})
